e3ba6df18845fc0cd256741971e45e69e0666618
MalikParvez1 <parvez_malik@outlook.de>
2024-07-23 13:00:30 +0200

    add cuton SW

--- vite.config.js
+++ vite.config.js
@@ -1,57 +1,45 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: 'auto',
-
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
-    manifest: {
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
         short_name: 'Vue PWA',
         name: 'Vue PWA',
         icons: [
           {
             src: 'logo192.png',
             type: 'image/png',
-            sizes: '192x192'
+            sizes: '192x192',
           },
           {
             src: 'logo512.png',
             type: 'image/png',
-            sizes: '512x512'
-          }
+            sizes: '512x512',
+          },
         ],
         start_url: '.',
         display: 'standalone',
         theme_color: '#5f9ea0',
-        background_color: '#5f9ea0'
-      ,
-    },
-
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-      runtimeCaching: [
-        {
-          urlPattern: /^https:\/\/api\.example\.com\/.*/i,
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'api-cache',
-            expiration: {
-              maxEntries: 10,
-              maxAgeSeconds: 60 * 60 * 24 * 365
+        background_color: '#5f9ea0',
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'image',
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'images',
+              expiration: {
+                maxEntries: 50,
+              },
             },
-            cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        }
-      ]
-    },
-  })],
-})
+          },
+        ],
+      },
+    }),
+  ],
+});
